ddb249c726277a09e2a8944b639290751fe71406
Herdy Handoko <herdy.handoko@hotmail.com>
2014-07-12 16:39:13 +0800

    Added MapBox libraries.

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -9,6 +9,8 @@ angular.module('botaniser.controllers', [])
 
 .controller('SpeciesDetailCtrl', function($scope, $stateParams, SpeciesList) {
     $scope.species = SpeciesList.get($stateParams.speciesId);
+
+    L.mapbox.map('map', 'examples.map-i86nkdio').setView([40, -74.50], 9);
 })
 
 .controller('AccountCtrl', function($scope) {
